ed974fbbc72b56b91c522efb1d172daea4359450
Jérémie GASTON-RAOUL <jeremie.gaston-raoul@dryade.net>
2012-03-05 08:54:26 +0100

    implementation of webtopo server

---
+++ webtopo-server/src/main/java/net/dryade/siri/webtopo/server/producer/GetTopologyVersion.java
@@ -0,0 +1,66 @@
+/**
+ * Siri Product - Produit SIRI
+ *
+ * a set of tools for easy application building with respect of the France Siri
+ * Local Agreement
+ *
+ * un ensemble d'outils facilitant la realisation d'applications respectant le
+ * profil France de la norme SIRI
+ *
+ * Copyright DRYADE 2009-2010
+ */
+package net.dryade.siri.webtopo.server.producer;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Collection;
+import net.dryade.siri.common.SiriException;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.filefilter.*;
+import webtopo.xsd.GetTopologyVersionDocument;
+import webtopo.xsd.GetTopologyVersionResponseDocument;
+import webtopo.xsd.TopologyVersionType;
+
+/**
+ * Implémentation du service GetTopologyVersion
+ */
+public class GetTopologyVersion implements GetTopologyVersionInterface {
+
+    private String topologyDirectory;
+    private String topologyWorkingVersion;
+
+    public GetTopologyVersion() throws SiriException {
+        super();
+        // initialiser ici les ressources
+    }
+
+    public GetTopologyVersionResponseDocument getTopologyVersion(GetTopologyVersionDocument requestDoc) {
+        GetTopologyVersionResponseDocument responseDoc = GetTopologyVersionResponseDocument.Factory.newInstance();
+        TopologyVersionType response = responseDoc.addNewGetTopologyVersionResponse();
+        File dir = new File(topologyDirectory);
+        if (dir.exists()) {
+            IOFileFilter filter = new AndFileFilter(DirectoryFileFilter.INSTANCE, new NotFileFilter(new NameFileFilter(topologyWorkingVersion)));
+            String[] listVersion = dir.list(filter);
+            if (listVersion.length == 0) {
+                response.setTopologyAvailable(false);
+            } else {
+                Arrays.sort(listVersion);
+                response.setTopologyAvailable(true);
+                response.setVersion(listVersion[listVersion.length - 1]);
+            }
+            if (requestDoc.getGetTopologyVersion().isSetLineNameList()) {
+                if (requestDoc.getGetTopologyVersion().getLineNameList()) {
+                    Collection<File> list = FileUtils.listFiles(new File(dir,response.getVersion()), new String[]{"xml"}, false);
+                    for (File file : list) {
+                        response.addAvailableLineList(FilenameUtils.getBaseName(file.getName()));
+                    }
+                }
+            }
+        } else {
+            response.setTopologyAvailable(false);
+        }
+        return responseDoc;
+
+    }
+}
---
+++ webtopo-server/src/main/java/net/dryade/siri/webtopo/server/producer/GetTopologyVersionInterface.java
@@ -0,0 +1,29 @@
+/**
+ *   Siri Product - Produit SIRI
+ *  
+ *   a set of tools for easy application building with 
+ *   respect of the France Siri Local Agreement
+ *
+ *   un ensemble d'outils facilitant la realisation d'applications
+ *   respectant le profil France de la norme SIRI
+ * 
+ *   Copyright DRYADE 2009-2010
+ */
+package net.dryade.siri.webtopo.server.producer;
+
+import webtopo.xsd.GetTopologyVersionDocument;
+import webtopo.xsd.GetTopologyVersionResponseDocument;
+
+/**
+ * 
+ */
+public interface GetTopologyVersionInterface
+{
+
+   /**
+    * @param request 
+   * @return
+    */
+   GetTopologyVersionResponseDocument getTopologyVersion(GetTopologyVersionDocument request);
+
+}
